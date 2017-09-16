eb778dab654662acfa5f8007675963076c1ae2f1
Justin Lee <leejwg.3@gmail.com>
2017-09-16 10:31:11 -1000

    test comit

--- mergeSort.js
+++ mergeSort.js
@@ -1,1 +1,20 @@
 console.log("linked");
+
+
+console.log('starting merge sort');
+
+var list = [1,4,3,100,200,300,7,9,26,33,99,77,66,55,40,34,27,140,10,16,19];
+var pointer= Math.floor(list.length/2)
+var leftSort= [];
+var rightSort=[];
+
+
+function mergeSort(list, leftSort,rightSort){
+  if(leftSort>rightSort){
+    return;
+  }
+  var middle = (leftSort + rightSort) / 2;
+  mergeSort(list, leftSort, middle);
+  mergeSort(list ,middle + 1,  rightSort);
+
+};
